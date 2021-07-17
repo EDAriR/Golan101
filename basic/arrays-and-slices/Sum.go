549c93cb084d9d50cca36a84c1920ea8a3549430
ED <ed.ho@thinkpower-info.com>
2021-07-18 02:23:43 +0900

    refactor array to slic

--- basic/arrays-and-slices/Sum.go
+++ basic/arrays-and-slices/Sum.go
@@ -1,5 +1,24 @@
 package main
 
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+/*
+func Sum(numbers [5]int) int {
+    sum := 0
+    for _, number := range numbers {
+        sum += number
+    }
+    return sum
+}
+*/
+
+/*
 func Sum(numbers [5]int) int {
 	sum := 0
 	for i := 0; i < 5; i++ {
@@ -7,3 +26,11 @@ func Sum(numbers [5]int) int {
 	}
 	return sum
 }
+*/
+
+/*
+数组有一个有趣的属性，它的大小也属于类型的一部分，如果你尝试将 [4]int 作为 [5]int 类型的参数传入函数，是不能通过编译的。它们是不同的类型，就像尝试将 string 当做 int 类型的参数传入函数一样。
+因为这个原因，所以数组比较笨重，大多数情况下我们都不会使用它。
+Go 的切片（slice）类型不会将集合的长度保存在类型中，因此它的尺寸可以是不固定的。
+
+*/
