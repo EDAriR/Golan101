bd33e2dc811c67a2149a35d92f6e7cc2cfab44a5
ED <ed.ho@thinkpower-info.com>
2021-07-23 01:17:01 +0900

    append & slice bounds out of range

--- basic/arrays-and-slices/Sum.go
+++ basic/arrays-and-slices/Sum.go
@@ -8,25 +8,28 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-/*
-func Sum(numbers [5]int) int {
-    sum := 0
-    for _, number := range numbers {
-        sum += number
-    }
-    return sum
-}
-*/
-
-/*
-func Sum(numbers [5]int) int {
-	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
-	return sum
+
+	return sums
 }
-*/
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
 
 /*
 数组有一个有趣的属性，它的大小也属于类型的一部分，如果你尝试将 [4]int 作为 [5]int 类型的参数传入函数，是不能通过编译的。它们是不同的类型，就像尝试将 string 当做 int 类型的参数传入函数一样。
