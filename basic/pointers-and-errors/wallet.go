f35b998e3932dfd16238d60392bfc07c80634f6a
ED <ed.ho@thinkpower-info.com>
2021-07-24 02:33:14 +0900

    error

--- basic/pointers-and-errors/wallet.go
+++ basic/pointers-and-errors/wallet.go
@@ -1,15 +1,39 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
 type Wallet struct {
-	balance int
+	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+	return nil
 }
 
 // 不同之处在于，接收者类型是 *Wallet 而不是 Wallet，你可以将其解读为「指向 wallet 的指针」。
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
