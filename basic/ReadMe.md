64484d60170ef340f8af67c766f6521baeb2e807
ED <ed.ho@thinkpower-info.com>
2021-07-24 02:00:53 +0900

    table driven test

---
+++ basic/ReadMe.md
@@ -0,0 +1,10 @@
+檔案名 xxx.go
+測試檔案命名 xxx_test.go
+
+執行 go run xxx.go 
+測試 go test
+运行列表中指定的测试用例： go test -run TestArea/Rectangle
+
+
+table driven test
+https://github.com/golang/go/wiki/TableDrivenTests
--- basic/structs-methods-and-interfaces/shapes.go
+++ basic/structs-methods-and-interfaces/shapes.go
@@ -2,6 +2,19 @@ package main
 
 import "math"
 
+/*
+这种定义 interface 的方式与大部分其他编程语言不同。通常接口定义需要这样的代码 My type Foo implements interface Bar。
+但是在我们的例子里，
+Rectangle 有一个返回值类型为 float64 的方法 Area，所以它满足接口 Shape
+Circle 有一个返回值类型为 float64 的方法 Area，所以它满足接口 Shape
+string 没有这种方法，所以它不满足这个接口
+等等
+在 Go 语言中 interface resolution 是隐式的。如果传入的类型匹配接口需要的，则编译正确。
+*/
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -11,8 +24,9 @@ type Circle struct {
 	Radius float64
 }
 
-type Shape interface {
-	Area() float64
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
@@ -25,4 +39,8 @@ func (c Circle) Area() float64 {
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (c Triangle) Area() float64 {
+	return (c.Base * c.Height) * 0.5
 }
