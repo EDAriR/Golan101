7de8d90bd0b78285881991b3b67c6feb039d0dc8
ED <ed.ho@thinkpower-info.com>
2021-07-24 22:51:38 +0900

    map create/update delete

--- basic/maps/dictionary.go
+++ basic/maps/dictionary.go
@@ -1,10 +1,28 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+/*
+dictionary = map[string]string{}
+
+// OR
+
+dictionary = make(map[string]string)
+
+这两种方法都可以创建一个空的 hash map 并指向 dictionary。这确保永远不会获得 nil 指针异常
+*/
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -13,4 +31,38 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
